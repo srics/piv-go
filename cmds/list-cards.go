baa0bb89775c80057522523308242d30edf5aefe
Sridhar Ramaswamy <sridhar@ves.io>
2021-03-09 16:49:59 -0800

    Print version and serial number

--- cmds/list-cards.go
+++ cmds/list-cards.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/go-piv/piv-go/piv"
+	"strings"
 )
 
 func main() {
@@ -13,5 +14,19 @@ func main() {
 
 	for i, card := range cards {
 		fmt.Printf("Card %d: %s\n", i, card)
+
+		if !strings.Contains(strings.ToLower(card), "yubikey") {
+			continue
+		}
+		yk, err := piv.Open(card)
+		if err != nil {
+			fmt.Printf("Error opening card: %s\n", err.Error())
+			continue
+		}
+		fmt.Printf("  Version: %d.%d.%d\n", yk.Version().Major, yk.Version().Minor, yk.Version().Patch)
+		serial, err := yk.Serial()
+		if err == nil {
+			fmt.Printf("  Serial: %d\n", serial)
+		}
 	}
 }
